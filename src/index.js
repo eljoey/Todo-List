12a2d318d81673e72c6d1bf73f90f437b2d05f21
Joe Hayden <eljoeyy@gmail.com>
2019-08-10 12:37:45 -0500

    add todo add button functionality

--- src/index.js
+++ src/index.js
@@ -5,12 +5,15 @@
 
 
 
+
 let projectDirectory = [];
 
 const projName = document.querySelector('.proj-form-name')
 const projDesc = document.querySelector('.proj-form-desc')
 const projDate = document.querySelector('.proj-form-date')
 const projPrio = document.querySelector('.proj-form-prio')
+const todoName = document.querySelector('.todo-form-name')
+const todoDesc = document.querySelector('.todo-form-desc')
 
 
 
@@ -38,19 +41,23 @@ const Project = (name, description, dueDate, priority) => {
 const DisplayController = (() => {
     const renderProjectBar = () => {
         resetProjects();
-        resetProjectForm();
+        resetForms();
 
         const projectMainDiv = document.querySelector('.projects')
 
         projectDirectory.forEach((x) => {
             let projectAdded = document.createElement('div');
             projectAdded.innerHTML = x.name;
-            projectAdded.classList.add('project-' + projectDirectory.indexOf(x)) 
+            projectAdded.classList.add('project-' + projectDirectory.indexOf(x))
+            projectAdded.addEventListener('click', () => {
+                EventHandler.showProjectTodos(projectDirectory.indexOf(x))
+            }); 
             projectMainDiv.appendChild(projectAdded)
-        })
+        });
     }
     const renderTodoList = (project) => {
         resetToDos();
+        resetForms();
 
         const todoDiv = document.querySelector('#right-section');
         
@@ -104,17 +111,17 @@ const DisplayController = (() => {
         });
     }
 
-    const resetProjectForm = () => {
-        let projName = document.querySelector('.proj-form-name')
-        let projDesc = document.querySelector('.proj-form-desc')
-        let projDate = document.querySelector('.proj-form-date')
-        let projPrio = document.querySelector('.proj-form-prio')
-
+    const resetForms = () => {
         projName.value = '';
         projDesc.value = '';
         projDate.value = '';
         projPrio.value = 'base';
+
+        todoName.value = '';
+        todoDesc.value = '';
     }
+
+    
 
 
     return {
@@ -123,7 +130,6 @@ const DisplayController = (() => {
         showAddProjectForm,
         showAddToDoForm,
         hideForm,
-        resetToDos,
         
     }
 })();
@@ -136,17 +142,16 @@ const EventListeners = (() => {
         addToDoBTN();
         exitForm();
         projectFormBTN();
+        todoFormBTN();
     }
 
     const addProjectForm = () => {
         const projectBTN = document.querySelector('.addProjectBTN')
-        projectBTN.addEventListener('click', DisplayController.showAddProjectForm)
-        
+        projectBTN.addEventListener('click', DisplayController.showAddProjectForm)        
     }
+
 //Gets Projects information and sets it.  I feel like this could be better set up.
     const projectFormBTN = () => {
-        
-
         const projectFormBTN = document.querySelector('.add-project-button');
         projectFormBTN.addEventListener('click', () => {
             let projNameValue = projName.value
@@ -160,7 +165,7 @@ const EventListeners = (() => {
     
 //Changes form back to hidden ONLY when the background is selected not the form div
     const exitForm = () => {
-        const formBackground = document.querySelectorAll('.hidden')
+        const formBackground = document.querySelectorAll('.hidden');
         formBackground.forEach((x) => {
             x.addEventListener('click', (e) => {
                 if (e.currentTarget == e.target) {
@@ -168,6 +173,17 @@ const EventListeners = (() => {
                 }
             }, false);
         });        
+    }
+
+//Gets ToDo form info and adds it to the currently selected project
+    const todoFormBTN = () => {
+        const todoBTN = document.querySelector('.add-todo-button');
+        todoBTN.addEventListener('click', () => {
+            let todoNameValue = todoName.value
+            let todoDescValue = todoDesc.value
+
+            EventHandler.addToDo(todoNameValue, todoDescValue);
+        });
     }
 
     const addToDoBTN = () => {
@@ -186,18 +202,39 @@ const EventListeners = (() => {
 ////////////////////////////////////
 
 const EventHandler = (() => {
-    
+    let projectSelected = projectDirectory[0];
+
     const addProject = (name, description, dueDate, priority) => {
         let newProject = Project(name, description, dueDate, priority);
         projectDirectory.push(newProject);
         DisplayController.renderProjectBar();
-        // DisplayController.resetProjectForm();
+        DisplayController.hideForm();
     };
+
+    const addToDo = (name, desc) => {
+        if (projectSelected === undefined) projectSelected = projectDirectory[0]
+        
+        let newToDo = ToDo(name, desc)
+        projectSelected.addToDo(newToDo);
+        DisplayController.renderTodoList(projectSelected); 
+        DisplayController.hideForm();       
+    }
+
+    const showProjectTodos = (projIndex) => {
+        projectSelected = projectDirectory[projIndex];
+
+        DisplayController.renderTodoList(projectSelected);
+    }
+    
 
     
 
     return {
         addProject,
+        addToDo,
+        projectSelected,
+        showProjectTodos,
+        
     }
 })();
 
@@ -206,6 +243,7 @@ const EventHandler = (() => {
 
 
 //test
+//////////////
 EventHandler.addProject('Daily list', 'Things to do today', '', 'low');
 EventHandler.addProject('Daily list2', 'Things to do today', '', 'low');
 EventHandler.addProject('Daily list3', 'Things to do today', '', 'low');
@@ -220,7 +258,7 @@ projectDirectory[1].addToDo(ToDo(123, 2))
 DisplayController.renderProjectBar();
 DisplayController.renderTodoList(projectDirectory[0]);
 EventListeners.initializeListeners();
-
+///////////////
 // export { projectDirectory }
 
 
@@ -235,3 +273,14 @@ EventListeners.initializeListeners();
 //-Add - Possibly create a visual for each project to display the days left until their due (probably wont add and just delete due date)
 //-Add - ToDo create and delete form
 //-Add - Project delete form
+
+
+
+//project selction
+//1. add event listener to each project added
+//2. event listener uses function to set project shown and then display the list of todo's
+//
+//
+//
+//
+//
